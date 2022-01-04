d39fa05ece0379c2848c7557a27a796756602cd8
Antoxa <toha-kartoha@Ksuhas-MacBook-Air.local>
2022-01-04 16:33:16 +0700

    first-commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+import "./App.css";
+
+const App = () => {
+  const [account, setAccount] = useState(null);
+
+  const connectWalet = async () => {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      console.log(accounts[0])
+      setAccount(accounts[0])
+    }
+  }
+
+  const renderNotConnected = () => (
+    <div>
+      <button onClick={connectWalet}>Connect Wallet</button>
+    </div>
+  )
+
+  const renderConnected = () => (
+    <div>
+      <h3>Here are some pastas...</h3>
+    </div>
+  )
+
+  return(
+    <div>
+      <h1>Pastalavista</h1>
+      <h2>The meme dictionary of web3</h2>
+      {!account && renderNotConnected()}
+      {account && renderConnected()}
+    </div>
+  )
+}
+
+export default App;
